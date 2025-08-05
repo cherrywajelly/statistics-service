85508582cee119129188ac7e64e1138b04216e2b
yeiin <yeini0417@gmail.com>
2025-08-05 20:39:26 +0900

    :sparkles: add stream listener and store new signup statistics

--- src/main/java/com/timetoast/statistics_service/member/application/MemberJoinService.java
+++ src/main/java/com/timetoast/statistics_service/member/application/MemberJoinService.java
@@ -3,9 +3,11 @@ package com.timetoast.statistics_service.member.application;
 import com.timetoast.statistics_service.member.port.in.MemberJoinUseCase;
 import com.timetoast.statistics_service.member.port.out.MemberStore;
 import com.timetoast.statistics_service.member.domain.dto.MemberJoinDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class MemberJoinService implements MemberJoinUseCase {
     private final MemberStore memberStore;
 
@@ -16,5 +18,6 @@ public class MemberJoinService implements MemberJoinUseCase {
     @Override
     public void saveJoinedMemberStats(MemberJoinDto dto) {
         memberStore.saveJoinedMemberStats(dto);
+        log.info("saved joined member stats id={}", dto.memberId());
     }
 }
